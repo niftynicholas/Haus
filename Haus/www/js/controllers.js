14884490515a5ab6573dfed176cdd930f798de95
Nicholas Tan Joo Sin <nicholas.tan.92@hotmail.com>
2016-10-17 15:51:48 +0800

    - Initial commit

---
+++ Haus/www/js/controllers.js
@@ -0,0 +1,36 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $state) {
+    $scope.logout = function() {
+        $state.go('login');
+    }
+})
+
+.controller('loginCtrl', function($scope, $state) {
+  $scope.login = function() {
+      $state.go('app.profile');
+  }
+})
+
+.controller('profileCtrl', function($scope, $state) {
+
+})
+
+.controller('setupCtrl', function($scope, $state, $ionicHistory) {
+    $scope.data = {
+        type: "single",
+        age: 21,
+        residency: "spore",
+        property: "hdb",
+        term: 20
+    };
+    $scope.goBack = function() {
+        $ionicHistory.goBack();
+    }
+})
+
+.controller('reportCtrl', function($scope, $state, $ionicHistory) {
+    $scope.goBack = function() {
+        $ionicHistory.goBack();
+    }
+});
