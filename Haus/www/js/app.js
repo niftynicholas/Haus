07b440faa77979cd665b872afdaffebd9c910f6a
Leong Wei Kong <wkleong.2014@sis.smu.edu.sg>
2016-10-19 16:57:35 +0800

    - Committing Backand setup codes

--- Haus/www/js/app.js
+++ Haus/www/js/app.js
@@ -4,94 +4,164 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'backand'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
+})
+
+.config(function (BackandProvider, $stateProvider, $urlRouterProvider, $httpProvider) {
+    BackandProvider.setAppName('haus');
+    BackandProvider.setSignUpToken('269ac5cd-5949-4e19-9cbe-50feb489c366');
+    BackandProvider.setAnonymousToken('b109c358-b8fa-49da-adce-6e3ce78569f2');
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
 
-  .state('landing', {
-    url: '/landing',
-    templateUrl: 'templates/landing.html'
-  })
+    .state('landing', {
+        url: '/landing',
+        templateUrl: 'templates/landing.html'
+    })
 
-  .state('login', {
-    url: '/login',
-    templateUrl: 'templates/login.html',
-    controller: 'loginCtrl'
-  })
+    .state('login', {
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+    })
 
-  .state('app.profile', {
-    url: '/profile',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/profile.html',
-        controller: 'profileCtrl'
-      }
-    }
-  })
+    .state('signup', {
+        url: '/signup',
+        templateUrl: 'templates/signup.html',
+        controller: 'signupCtrl'
+    })
 
-  .state('setup', {
-    url: '/setup',
-    templateUrl: 'templates/setup.html',
-    controller: 'setupCtrl'
-  })
+    .state('app.profile', {
+        url: '/profile',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/profile.html',
+                controller: 'profileCtrl'
+            }
+        }
+    })
 
-  .state('report', {
-    url: '/report',
-    templateUrl: 'templates/report.html',
-    controller: 'reportCtrl'
-  })
+    .state('setup', {
+        url: '/setup',
+        templateUrl: 'templates/setup.html',
+        controller: 'setupCtrl'
+    })
 
-  .state('app.browseLoans', {
-    url: '/browseLoans',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/browseLoans.html'
-      }
-    }
-  })
+    .state('report', {
+        url: '/report',
+        templateUrl: 'templates/report.html',
+        controller: 'reportCtrl'
+    })
 
-  .state('app.browseProperties', {
-    url: '/browseProperties',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/browseProperties.html'
-      }
-    }
-  })
+    .state('app.browseLoans', {
+        url: '/browseLoans',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/browseLoans.html'
+            }
+        }
+    })
 
-  .state('app.scheduleAppt', {
-    url: '/scheduleAppt',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/scheduleAppt.html',
-        controller: 'scheduleApptCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/landing');
+    .state('app.browseProperties', {
+        url: '/browseProperties',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/browseProperties.html'
+            }
+        }
+    })
+
+    .state('app.scheduleAppt', {
+        url: '/scheduleAppt',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/scheduleAppt.html',
+                controller: 'scheduleApptCtrl'
+            }
+        }
+    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/landing');
 });
+
+//Manage items object in /www/js/services.js
+// .service('ItemsModel', function ($http, Backand) {
+//     var service = this,
+//     baseUrl = '/1/objects/',
+//     objectName = 'items/';
+//
+//     ...
+// })
+
+.service('itemDAO', function($http, Backand) {
+    //https://api.backand.com:443/1/objects/users?returnObject=false&deep=false
+    var service = {};
+    var baseUrl = '/1/objects/',
+    objectName = 'items/';
+
+    function getUrl() {
+        return Backand.getApiUrl() + baseUrl + objectName;
+    }
+
+    function getUrlForId(id) {
+        return getUrl() + id;
+    }
+
+    service.getUsers = function() {
+        return $http.get(getUrl());
+    };
+
+    service.getUser = function(id) {
+        return $http.get(getUrlForId(id));
+    };
+
+    service.addUser = function(object) {
+        console.log(object);
+        return $http.post(getUrl(), object);
+    };
+
+    service.updateUser = function(id, object) {
+        return $http.put(getUrlForId(id), object);
+    };
+
+    service.deleteUser = function(id) {
+        return $http.delete(getUrlForId(id));
+    };
+    // CUSTOM QUERY
+    // service.getUserByUsername = function(username) {
+    //     return $http({
+    //         method: 'GET',
+    //         url: Backand.getApiUrl() + '/1/query/data/getUserByUsername',
+    //         params: {
+    //             parameters: {
+    //                 username: username
+    //             }
+    //         }
+    //     });
+    // }
+    return service;
+})
