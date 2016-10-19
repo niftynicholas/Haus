f0ec7cdbb6e178d99570efdd1e569d14785711a1
Nicholas Tan Joo Sin <nicholas.tan.92@hotmail.com>
2016-10-19 16:56:44 +0800

    - Added in sign up page.

--- Haus/www/js/controllers.js
+++ Haus/www/js/controllers.js
@@ -1,14 +1,30 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $state) {
-    $scope.logout = function() {
-        $state.go('landing');
-    }
+  $scope.logout = function() {
+    $state.go('landing');
+  }
 })
 
 .controller('loginCtrl', function($scope, $state) {
   $scope.login = function() {
-      $state.go('app.profile');
+    $state.go('app.profile');
+  }
+})
+
+.controller('signupCtrl', function($scope, $state, $ionicHistory) {
+  $scope.type = "password";
+
+  $scope.toggleVisibility = function() {
+    if ($scope.type == "password") {
+      $scope.type = "text";
+    } else if ($scope.type == "text") {
+      $scope.type = "password";
+    }
+  }
+
+  $scope.goBack = function() {
+    $ionicHistory.goBack();
   }
 })
 
@@ -17,26 +33,39 @@ angular.module('starter.controllers', [])
 })
 
 .controller('setupCtrl', function($scope, $state, $ionicHistory) {
-    $scope.data = {
-        type: "single",
-        age: 21,
-        residency: "spore",
-        property: "hdb",
-        term: 20
-    };
-    $scope.goBack = function() {
-        $ionicHistory.goBack();
-    }
+  $scope.data = {
+    type: "single",
+    age: 21,
+    residency: "spore",
+    property: "hdb",
+    term: 20
+  };
+  $scope.goBack = function() {
+    $ionicHistory.goBack();
+  }
 })
 
 .controller('reportCtrl', function($scope, $state, $ionicHistory) {
-    $scope.goBack = function() {
-        $ionicHistory.goBack();
-    }
+  $scope.goBack = function() {
+    $ionicHistory.goBack();
+  }
 })
 
-.controller('scheduleApptCtrl', function($scope, $state, $ionicHistory) {
-    $scope.goBack = function() {
-        $ionicHistory.goBack();
-    }
+.controller('scheduleApptCtrl', function($scope, $state, $ionicHistory, $ionicModal) {
+  $scope.goBack = function() {
+    $ionicHistory.goBack();
+  }
+
+  $ionicModal.fromTemplateUrl('bankModal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.bankModal = modal;
+  });
+  $scope.openBankModal = function() {
+    $scope.bankModal.show();
+  };
+  $scope.closeBankModal = function() {
+    $scope.bankModal.hide();
+  };
 });
